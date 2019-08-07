9a3ec513e5bd8d22f07c3d63e87cd85735cf17b5
xzwb <1478863479@qq.com>
2019-08-07 11:19:06 +0800

    处理了登录时输错密码和账号的问题,登录模块ok

--- 聊天室/main.c
+++ 聊天室/main.c
@@ -2,92 +2,103 @@
 #include <stdio.h>
 #include <sys/epoll.h>
 #include <pthread.h>
-#include "my_err.h"
-#include "my_socket.h"
-#include "my_login.h"
-#include "my_mysql.h"
-#include "my_send.h"
+#include "my_friends.h"
 #include "my_deal.h"
-
+#include "my_mysql.h"
+#include "my_socket.h"
+#include <unistd.h>
+#include "my_err.h"
+#include "my_pack.h"
 
 #define MAXEPOLL 1024
 
 
-int main()
-{
-	int                        i;
-	int                        sock_fd;
-	int                        conn_fd;
-	int                        socklen;
-	int                        acceptcont = 0;
-	int                        kdpfd;
-	int                        curfds;
-	int                        nfds;
-	char                       need[MAXIN];
-	MYSQL                      mysql;
-	struct sockaddr_in         cli;
-	struct epoll_event         ev;
-	struct epoll_event         events[MAXEPOLL];
-	PACK                       recv_pack;
-	PACK                       *pack;
-	pthread_t                  pid;
+int main() {
+	 int                        i;
+	 int                        sock_fd;
+	 int                        conn_fd;
+	 int                        socklen;
+	 int                        acceptcont = 0;
+	 int                        kdpfd;
+	 int                        curfds;
+	 int                        nfds;
+	 char                       need[MAXIN];
+	 MYSQL                      mysql;
+	 struct sockaddr_in         cli;
+	 struct epoll_event         ev;
+	 struct epoll_event         events[MAXEPOLL];
+	 PACK                       recv_pack;
+	 PACK                       *pack;
+	 pthread_t                  pid;
+     MYSQL_RES                  *result;
+    
+     pthread_mutex_init(&mutex, NULL);
+	 socklen = sizeof(struct sockaddr_in);
+	 mysql = accept_mysql();
+	 sock_fd = my_accept_seve();
 
-	socklen = sizeof(struct sockaddr_in);
-	mysql = accept_mysql();
-	sock_fd = my_accept_seve();
+	 kdpfd = epoll_create(MAXEPOLL);
 
-	kdpfd = epoll_create(MAXEPOLL);
+	 ev.events = EPOLLIN | EPOLLET;
+	 ev.data.fd = sock_fd;
 
-	ev.events = EPOLLIN | EPOLLET;
-	ev.data.fd = sock_fd;
+	 if(epoll_ctl(kdpfd, EPOLL_CTL_ADD, sock_fd, &ev) < 0) {
+	 	my_err("epoll_ctl", __LINE__);
+	 }
 
-	if(epoll_ctl(kdpfd, EPOLL_CTL_ADD, sock_fd, &ev) < 0){
-		my_err("epoll_ctl", __LINE__);
-	}
+	 curfds = 1;
 
-	curfds = 1;
+	while(1) {
+	 	if((nfds = epoll_wait(kdpfd, events, curfds, -1)) < 0){
+	 		my_err("epoll_wait", __LINE__);
+	 }
 
-	while(1){
-		if((nfds = epoll_wait(kdpfd, events, curfds, -1)) < 0){
-			my_err("epoll_wait", __LINE__);
-		}
+ 	for (i = 0; i < nfds; i++) { 
+		if (events[i].data.fd == sock_fd) {
+ 			if ((conn_fd = accept(sock_fd, (struct sockaddr*)&cli, &socklen)) < 0) {
+ 				my_err("accept", __LINE__);
+ 			}
+ 			printf("连接成功,套接字编号%d\n", conn_fd);
+ 			acceptcont++;
 
-		for(i = 0; i < nfds; i++){
-			if(events[i].data.fd == sock_fd){
-				if((conn_fd = accept(sock_fd, (struct sockaddr*)&cli, &socklen)) < 0){
-					my_err("accept", __LINE__);
-				}
-				printf("连接成功,套接字编号%d\n", conn_fd);
-				acceptcont++;
+ 			ev.events = EPOLLIN | EPOLLET;
+ 			ev.data.fd = conn_fd;
 
-				ev.events = EPOLLIN | EPOLLET;
-				ev.data.fd = conn_fd;
-
-				if(epoll_ctl(kdpfd, EPOLL_CTL_ADD, conn_fd, &ev) < 0){
-					my_err("epoll_ctl", __LINE__);
-				}
-				curfds++;
-				continue;
-			}
-			else if(events[i].events & EPOLLIN){
-				memset(&recv_pack, 0, sizeof(PACK));
-				if(recv(events[i].data.fd, &recv_pack, sizeof(PACK), 0) < 0){
-					close(events[i].data.fd);
-					perror("recv");
-					continue;
-				}
+ 			if (epoll_ctl(kdpfd, EPOLL_CTL_ADD, conn_fd, &ev) < 0) {
+ 				my_err("epoll_ctl", __LINE__);
+ 			}
+ 			curfds++;
+ 			continue;
+ 		} else if (events[i].events & EPOLLIN) { 
+			memset(&recv_pack, 0, sizeof(PACK));
+ 			if (recv(events[i].data.fd, &recv_pack, sizeof(PACK), 0) < 0) {
+ 				close(events[i].data.fd);
+ 				perror("recv");
+ 				continue;
+ 			}
 			        
-				memset(need, 0, sizeof(need));	
-				printf("star\n");
-				sprintf(need, "update user_data set user_socket = %d where account = %d", events[i].data.fd, recv_pack.data.send_account);
-				printf("\n%s\n",need);
-				printf("star\n");
-				mysql_query(&mysql, need);
-				pack = (PACK*)malloc(sizeof(PACK));
-				memcpy(pack, &recv_pack, sizeof(PACK));
-				pthread_create(&pid, NULL, deal, (void*)pack);
-
-			}
-		}
-	}
+ 			memset(need, 0, sizeof(need));
+            sprintf(need, "select *from user_data where account = %d", recv_pack.data.send_account);
+            pthread_mutex_lock(&mutex);
+            mysql_query(&mysql, need);
+            result = mysql_store_result(&mysql);
+            if (!mysql_fetch_row(result)) {
+                memset(recv_pack.data.write_buff, 0, sizeof(recv_pack.data.write_buff));
+                strcpy(recv_pack.data.write_buff, "password error");
+                if (send(events[i].data.fd, &recv_pack, sizeof(PACK), 0) < 0) {
+                    my_err("send", __LINE__);
+                }
+                pthread_mutex_unlock(&mutex);
+                continue;
+            }
+ 			memset(need, 0, sizeof(need));
+ 			sprintf(need, "update user_data set user_socket = %d where account = %d", events[i].data.fd, recv_pack.data.send_account);
+ 			mysql_query(&mysql, need);
+            pthread_mutex_unlock(&mutex);
+ 			pack = (PACK*)malloc(sizeof(PACK));
+ 			memcpy(pack, &recv_pack, sizeof(PACK));
+ 			pthread_create(&pid, NULL, deal, (void*)pack);
+            }
+         }
+    } 
 }
--- 聊天室/main_cli.c
+++ 聊天室/main_cli.c
@@ -2,19 +2,131 @@
 #include <pthread.h>
 #include "my_pack.h"
 #include "my_socket.h"
+#include "my_err.h"
+#include "my_mune.h"
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+PACK *send_pack;
+PACK *recv_pack;
+
+/* 用来发送数据的线程 */
+void *thread_read(void *sock_fd) {
+    int               choose;
+    char              password[20];
+    
+    send_pack = (PACK *)malloc(sizeof(PACK));
+    /* 套接字编号放进结构体 */
+    send_pack->data.send_fd = *(int *)sock_fd;
+    /* 1为登录,2为注册,3为退出 */
+    while (1) {
+        login_mune();
+        scanf("%d", &choose);
+        switch (choose) {
+            case 1:
+                   {
+                       send_pack->type = LOGIN;
+                       printf("请输入账号:\n");
+                       scanf("%d", &send_pack->data.send_account);
+                       getchar();
+                       printf("请输入密码:\n");
+                       scanf("%s", send_pack->data.read_buff);
+                       if (send(*(int *)sock_fd, send_pack, sizeof(PACK), 0) < 0) {
+                           my_err("send", __LINE__);
+                       }
+                       pthread_mutex_lock(&mutex_cli);
+                       pthread_cond_wait(&cond_cli, &mutex_cli);
+                       pthread_mutex_unlock(&mutex_cli);
+                       printf("%s\n", send_pack->data.write_buff);
+                       break;
+                   }
+           case 2:
+                   {
+                       send_pack->type = REGISTERED;
+                       printf("请输入昵称:\n");
+                       scanf("%s", send_pack->data.send_user);
+                       getchar();
+                       printf("请输入密码:\n");
+                       scanf("%s", send_pack->data.read_buff);
+                       getchar();
+                       printf("请再次输入密码:\n");
+                       scanf("%s", password);
+                       if (strcmp(password, send_pack->data.read_buff) == 0) {
+                           printf("两次输入一制OK\n");
+                           if (send(*(int *)sock_fd, send_pack, sizeof(PACK), 0) < 0) {
+                               my_err("send", __LINE__);
+                           }
+                           pthread_mutex_lock(&mutex_cli);
+                           pthread_cond_wait(&cond_cli, &mutex_cli);
+                           pthread_mutex_unlock(&mutex_cli); 
+                       }
+                       break;
+                   } 
+           default:
+                   {
+                       printf("狗东西,好好输入!!!\n");
+                       break;
+                   }
+        }
+        if (choose > 3 || choose < 1) {
+            continue;   
+        } else {
+            if (strcmp(send_pack->data.write_buff, "password error") == 0) {
+                printf("密码错误或账号错误!!\n按下回车继续.....");
+                getchar();
+                getchar();
+                continue;
+            } else {
+                break;
+            }
+        }
+    }
+}
+
+void *thread_write(void *sock_fd) {
+    recv_pack = (PACK*)malloc(sizeof(PACK));
+    while (1) {
+        memset(recv_pack, 0, sizeof(PACK));
+        if (recv(*(int *)sock_fd, recv_pack, sizeof(PACK), 0) < 0) {
+            my_err("recv", __LINE__);
+        }
+        switch(recv_pack->type) {
+            case LOGIN:
+                    {
+                        strcpy(send_pack->data.send_user, recv_pack->data.send_user);
+                        memset(send_pack->data.write_buff, 0, sizeof(send_pack->data.write_buff));
+                        strcpy(send_pack->data.write_buff, recv_pack->data.write_buff);
+                        pthread_mutex_lock(&mutex_cli);
+                        pthread_cond_signal(&cond_cli);
+                        pthread_mutex_unlock(&mutex_cli); 
+                        break; 
+                    }
+            case REGISTERED:
+                    {
+                        pthread_mutex_lock(&mutex_cli);
+                        send_pack->data.send_account = recv_pack->data.send_account;
+                        pthread_cond_signal(&cond_cli);
+                        pthread_mutex_unlock(&mutex);
+                    }
+        }
+    }
+}
 
 
-int main()
-{
+
+int main() {
 	int               sock_fd;
-	sock_fd = my_accept_cli();
-	PACK              pack;
-	pack.type = LOGIN;
-	pack.data.send_account = 1000000;
-	strcpy(pack.data.messages, "520520cw...");
-	getchar();
-	send(sock_fd, &pack, sizeof(PACK), 0);
-	while(1)
-	{}
+    pthread_t         pid1;
+    pthread_t         pid2;
+    struct sockaddr_in seve;
+	
+    pthread_mutex_init(&mutex_cli, NULL);
+    pthread_cond_init(&cond_cli, NULL);
+    sock_fd = my_accept_cli();
+    pthread_create(&pid1, NULL, thread_read, (void *)&sock_fd);
+    pthread_create(&pid2, NULL, thread_write, (void *)&sock_fd);
+    pthread_join(pid1, NULL);
+    pthread_join(pid2, NULL);
+    
 	return 0;
 }
